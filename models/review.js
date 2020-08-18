97a173739428eb9a2f68f082f05adc0a457661ab
Shaheer <officialshaheer@gmail.com>
2020-08-19 02:00:48 +0530

    Modeling Review database
    created the database schema for Review.js model.

--- models/review.js
+++ models/review.js
@@ -1,3 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+body: String,
+author: {
+	type: Schema.Types.ObjectID,
+	ref: 'User'
+}
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
+
+
+
 /*
 Review
 - Body - string 
