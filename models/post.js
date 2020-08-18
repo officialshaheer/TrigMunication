a79426921d5458f0dbc94e431b961c669c951adb
Shaheer <officialshaheer@gmail.com>
2020-08-19 01:41:15 +0530

    Database schema created for Post model
    models/post is created and defined database.

--- models/post.js
+++ models/post.js
@@ -1,3 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+title: String,
+price: String,
+description: String,
+images: [ String ],
+location: String,
+lat: Number,
+lng: Number,
+author: {
+	type: Schema.Types.ObjectID,
+	ref: 'User'
+},
+reviews: [
+	{
+		types: Schema.Types.ObjectId,
+		ref: 'Review'
+	}
+]
+});
+
+module.exports = mongoose.model('Post', PostSchema);
+
+
 /*
 Post
 - Title - String
